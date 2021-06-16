eb55e029bba86e9baaaa1de0effb7ed1765e13a2
Samodra <38998978+saamodra@users.noreply.github.com>
2021-06-17 01:00:48 +0700

    Initial

---
+++ src/main/java/com/rps/adagawe/AdagaweApplication.java
@@ -0,0 +1,13 @@
+package com.rps.adagawe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdagaweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdagaweApplication.class, args);
+    }
+
+}
