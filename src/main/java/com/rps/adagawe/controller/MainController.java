34a0f83c6723544fd78d00adf956ed37576346bc
Samodra <38998978+saamodra@users.noreply.github.com>
2021-06-30 19:36:31 +0700

    Update lowongan data

--- src/main/java/com/rps/adagawe/controller/MainController.java
+++ src/main/java/com/rps/adagawe/controller/MainController.java
@@ -1,14 +1,19 @@
 package com.rps.adagawe.controller;
 
+import com.rps.adagawe.repository.LowonganRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
 public class MainController {
+    @Autowired
+    private LowonganRepository lowonganRepository;
 
     private void redirectIndex(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -26,6 +31,13 @@ public class MainController {
     @GetMapping("/admin")
     public String index(Model model) {
         redirectIndex(model);
+        return "main/index-admin";
+    }
+
+    @GetMapping("/lowongan/{id}")
+    public String lowongan(@PathVariable("id") int id, Model model) {
+        
+
         return "main/index-admin";
     }
 
