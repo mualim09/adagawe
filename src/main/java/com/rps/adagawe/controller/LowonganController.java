4f5c0dcc080db407d53903985ca990183ec676b6
Mochammad Rifqy <muhrifh@gmail.com>
2021-06-30 19:33:37 +0700

    Add Index & Create
    Add 2 Pages, and update database

--- src/main/java/com/rps/adagawe/controller/LowonganController.java
+++ src/main/java/com/rps/adagawe/controller/LowonganController.java
@@ -1,7 +1,43 @@
 package com.rps.adagawe.controller;
 
+import com.rps.adagawe.helper.AdagaweMethods;
+import com.rps.adagawe.model.Jabatan;
+import com.rps.adagawe.model.Lowongan;
+import com.rps.adagawe.service.JabatanService;
+import com.rps.adagawe.service.JenisPegawaiService;
+import com.rps.adagawe.service.LowonganService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class LowonganController {
+
+    @Autowired
+    LowonganService lowonganService;
+
+    @Autowired
+    JenisPegawaiService jenisPegawaiService;
+
+    @GetMapping("/perusahaan/lowongan")
+    public String index(Model model, HttpServletRequest request) {
+        List<Lowongan> lowongans = lowonganService.getAll();
+        model.addAttribute("lowongans", lowongans);
+
+        model.addAttribute("url", AdagaweMethods.getMainUrl(request));
+        return "/lowongan/index";
+    }
+
+    @GetMapping("/perusahaan/lowongan/create")
+    public String getCreate(Model model, HttpServletRequest request) {
+        model.addAttribute("jenisPegawais", jenisPegawaiService.findJenisPegawaiByRowStatus());
+        model.addAttribute("lowongan", new Lowongan());
+
+        model.addAttribute("url", AdagaweMethods.getMainUrl(request));
+        return "/lowongan/create";
+    }
 }
--- src/main/java/com/rps/adagawe/service/LowonganService.java
+++ src/main/java/com/rps/adagawe/service/LowonganService.java
@@ -1,7 +1,21 @@
 package com.rps.adagawe.service;
 
+import com.rps.adagawe.model.Admin;
+import com.rps.adagawe.model.Lowongan;
+import com.rps.adagawe.repository.AdminRepository;
+import com.rps.adagawe.repository.LowonganRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LowonganService {
+
+    @Autowired
+    LowonganRepository lowonganRepository;
+
+    public List<Lowongan> getAll() {
+        return (List<Lowongan>) lowonganRepository.findAll();
+    }
 }
